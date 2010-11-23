06d294bdfa05ba416519ea29ba94f63c19faafb1
Juraj Jancuska <jjancuska@gmail.com>
2010-11-23 22:21:41 +0100

    added Eshop helper

--- config/eshop_bootstrap.php
+++ config/eshop_bootstrap.php
@@ -1,6 +1,7 @@
 <?php
         Croogo::hookBehavior('Node', 'Eshop.Eshop');
         Croogo::hookHelper('*', 'Number');
+        Croogo::hookHelper('Nodes', 'Eshop.Eshop');
         Croogo::hookAdminMenu('Eshop');
         Croogo::hookAdminTab('Nodes/admin_edit', 'Eshop', 'eshop.admin_tab_node', array('type' => array('product')));
 ?>
---
+++ views/helpers/eshop.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Eshop helper
+ *
+ *
+ * @package  Croogo
+ * @author Juraj Jancuska <jjancuska@gmail.com>
+ */
+class EshopHelper extends AppHelper {
+
+        /**
+         * Used helpers
+         *
+         * @var array
+         */
+        public $helpers = array(
+            'Html',
+            'Layout',
+        );
+
+        /**
+         * Add items element
+         *
+         * @return void
+         */
+        public function afterSetNode() {
+                if ($this->Layout->node['Node']['type'] == 'product') {
+                        $this->Layout->setNodeField('body',
+                                $this->Layout->node('title') . $this->Layout->View->element('eshop_items_form', array('items' => $this->Layout->node['EshopItems'], 'plugin' => 'eshop')));
+                }
+        }
+}?>
